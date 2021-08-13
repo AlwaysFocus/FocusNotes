2d310f80505960628a872dc7394952c89f577ac6
AlwaysFocus <eliwhalen3@gmail.com>
2021-08-13 08:52:57 -0600

    init

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError,
+  ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar');
+
 module.exports = {
   // Construct our resolver function
   newNote: async (parent, args, { models }) => {
@@ -31,5 +41,32 @@ module.exports = {
         new: true
       }
     );
+  },
+
+  signUp: async (parent, { username, email, password }, { models }) => {
+    // convert email to all lowercase and remove whitespace
+    email = email.trim().toLowerCase();
+    // hash password
+    const hashed = await bcrypt.hash(password, 10);
+
+    // create user avatar
+    const avatar = gravatar(email);
+
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        password: hashed,
+        avatar
+      });
+
+      // create and pass back JWT
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (error) {
+      console.log(error);
+      throw new Error(
+        'There was an error when attempting to create your account.'
+      );
+    }
   }
 };
